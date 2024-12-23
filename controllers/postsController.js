ff5c7f71fd554fe8f693fb3de9ae79ab0dde49a5
arafat-alim <arafat97alim@gmail.com>
2024-12-23 15:12:05 +0530

    ✔️ Restful APIs - Posts API: Implemented the getPosts api and working on other posts apis.

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,19 +1,77 @@
-exports.getAllPosts = async (req, res) => {
-  res.status(200).json({ success: true, message: "Good to go!" });
+const Post = require("../models/postsModel");
+
+exports.getPosts = async (req, res) => {
+  const { page = 1 } = req.query;
+  let postPerPage = 10;
+  let totalPosts;
+
+  try {
+    let pageNum = 0;
+    if (page <= 1) {
+      pageNum = 0;
+    } else {
+      pageNum = page - 1;
+    }
+
+    const results = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip(pageNum * postPerPage)
+      .limit(postPerPage)
+      .populate({
+        path: "UserId",
+        select: "email",
+      });
+
+    res.status(200).json({
+      success: true,
+      message: "posts",
+      data: results,
+      metadata: {
+        currentPage: page,
+        postsPerPage: postPerPage,
+        totalPosts: results.length,
+        totalPages: Math.ceil(totalPosts / postPerPage)
+          ? Math.ceil(totalPosts / postPerPage)
+          : 1,
+      },
+    });
+  } catch (err) {
+    console.log("Something went wrong with Get Posts API: ", err);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch posts. Please try again later.",
+    });
+  }
 };
 
-exports.getSinglePost = async (req, res) => {
+exports.getPost = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log("Something went wrong with Get Posts API: ", err);
+  }
   res.status(200).json({ success: true, message: "Showing single post" });
 };
 
 exports.createPost = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log("Something went wrong with Get Posts API: ", err);
+  }
   res.status(200).json({ success: true, message: "Posting" });
 };
 
 exports.updatePost = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log("Something went wrong with Get Posts API: ", err);
+  }
   res.status(200).json({ success: true, message: "Updated Post" });
 };
 
 exports.deletePost = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log("Something went wrong with Get Posts API: ", err);
+  }
   res.status(200).json({ success: true, message: "Delete Post" });
 };
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const postsController = require("../controllers/postsController.js");
+const { identifier } = require("../middelwares/identification.js");
 
 const postsRouter = express.Router();
 
-postsRouter.get("/posts", postsController.getAllPosts);
-postsRouter.get("/single-post", postsController.getSinglePost);
-postsRouter.post("/create-post", postsController.createPost);
-postsRouter.put("/update-post", postsController.updatePost);
-postsRouter.delete("/delete-post", postsController.deletePost);
+postsRouter.get("/posts", identifier, postsController.getPosts);
+postsRouter.get("/single-post", identifier, postsController.getPost);
+postsRouter.post("/create-post", identifier, postsController.createPost);
+postsRouter.put("/update-post", identifier, postsController.updatePost);
+postsRouter.delete("/delete-post", identifier, postsController.deletePost);
 
 module.exports = postsRouter;
