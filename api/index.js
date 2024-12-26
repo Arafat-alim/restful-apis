dcd243de5109c0e722b4713e52cfae65475f015b
arafat-alim <arafat97alim@gmail.com>
2024-12-26 16:59:41 +0530

    🌍 Hosting the express app - Files added and updated the old files.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-import app from "./index.js";
+const app = require("../app");
 
 export default app;
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const appRouter = require("./routers/authRouter");
+const postsRouter = require("./routers/postsRouter");
+// const { generateSecretKey } = require("./utils/generateJwtToken");
+
+const app = express();
+
+//! generating the crpto token for jwt secret key, only for one time
+// const secretKey = generateSecretKey();
+// console.log("Generated Secret Key:", secretKey);
+
+//! adding middleware
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json()); // ! for handling request and response in json
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo DB is connected"))
+  .catch((err) => console.log("Something went wrong: ", err));
+
+//! use appRouter
+app.use("/api/auth", appRouter);
+//! use postsRouter
+app.use("/api/post", postsRouter);
+//! create routers
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello From Express Server",
+  });
+});
+
+module.exports = app;
